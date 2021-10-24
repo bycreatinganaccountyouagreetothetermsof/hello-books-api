cfa4ba1aa1f2bc9229da4db302f1d46a6300d943
lain <lain.winslow@gmail.com>
2021-10-24 13:08:07 -0700

    added endpoint to GET all /books from db

--- app/routes.py
+++ app/routes.py
@@ -6,25 +6,25 @@ from flask import Blueprint, jsonify, make_response, request
 book_bp = Blueprint("book", __name__, url_prefix="/books")
 
 
-@book_bp.route("", methods=["POST"])
-def handle_books():
-    request_body = request.get_json()
-    new_book = Book(
-        title=request_body["title"], description=request_body["description"]
-    )
-    db.session.add(new_book)
-    db.session.commit()
-    return make_response(f"Book {new_book.title} successfully created", 201)
+@book_bp.route("", methods=["GET", "POST"])
+def handle_book():
+    if request.method == "GET":
+        return jsonify(
+            [
+                {"id": book.id, "title": book.title, "description": book.description}
+                for book in Book.query.all()
+            ]
+        )
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(
+            title=request_body["title"], description=request_body["description"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return make_response(f"Book {new_book.title} successfully created", 201)
 
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#    books_response = []
-#    for book in books:
-#        books_response.append(
-#            {"id": book.id, "title": book.title, "description": book.description}
-#        )
-#    return jsonify(books_response)
 #
 #
 # @books_bp.route("/<book_id>", methods=["GET"])
