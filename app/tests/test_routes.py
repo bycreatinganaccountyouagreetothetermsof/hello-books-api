e32e43110b46772dc2af1c624c504720ab4b3131
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-07 13:57:32 -0800

    tests from lesson

---
+++ app/tests/test_routes.py
@@ -0,0 +1,33 @@
+def test_get_all_books_with_no_records(client):
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_one_book_with_no_records(client):
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == None
+
+
+def test_get_all_books(client, two_saved_books):
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == [
+        {"id": 1, "title": "Ocean Book", "description": "watr 4evr"},
+        {"id": 2, "title": "Mountain Book", "description": "i luv 2 climb rocks"},
+    ]
+
+
+def test_get_one_book(client, two_saved_books):
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {"id": 1, "title": "Ocean Book", "description": "watr 4evr"}
